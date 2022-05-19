77bda5ef7ada91de487d6d4e194f99c7937b0ee6
PLZ <2436478810@qq.com>
2022-05-19 11:44:51 +0800

    增大前瞻距离到0.65；增大差速；效果不太行

--- 小车单片机/USER/Cpu1_Main.c
+++ 小车单片机/USER/Cpu1_Main.c
@@ -277,13 +277,17 @@ void core1_main(void)
             //进入条件：识别类型为9或者处于环岛，且误差变化率大的；处于环岛入口阶段的
             if (((Get_d_steering_Error()>30||Get_d_steering_Error()<-30) && (classification_Result == 9 || (flag_For_Right_Circle == 1 || flag_For_Left_Circle == 1) ) )  ||  (Read_Timer_Status(0) == RUNNING && (flag_For_Right_Circle == 1 || flag_For_Left_Circle == 1)) )
             {
-                Cal_Steering_Error(0.55);//根据Col_Center和扫描范围search_Lines计算误差（全局变量，待定义）
+                Cal_Steering_Error(0.65);//根据Col_Center和扫描范围search_Lines计算误差（全局变量，待定义）
                 speed_Target = speed_Target_Min;
 
                 Differential_Ratio = 1.2f;
                 if (speed_Target_Min >= 2.4f && speed_Target_Max >= 2.6f)//只有2.3/2.1以上才可以
                 {
                     Differential_Ratio = 1.3f;
+                }
+                if (speed_Target_Min >= 3.0f && speed_Target_Max >= 3.1f)//只有2.8/2.7以上才可以
+                {
+                    Differential_Ratio = 1.5f;
                 }
 
 
@@ -317,6 +321,10 @@ void core1_main(void)
                 if (speed_Target_Min >= 2.4f && speed_Target_Max >= 2.6f)//只有2.3/2.1以上才可以
                 {
                     Differential_Ratio = 1.2f;
+                }
+                if (speed_Target_Min >= 3.0f && speed_Target_Max >= 3.1f)//只有2.8/2.7以上才可以
+                {
+                    Differential_Ratio = 1.4f;
                 }
 
                 Change_Steering_PID(0.25f,0,0.30f);
