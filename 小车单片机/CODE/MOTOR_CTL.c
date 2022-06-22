12718c7cbd83e0f6a3b5a958d06ce330bd65864e
PLZ <2436478810@qq.com>
2022-06-22 14:37:07 +0800

    校赛版本

--- 小车单片机/CODE/MOTOR_CTL.c
+++ 小车单片机/CODE/MOTOR_CTL.c
@@ -27,6 +27,7 @@ void Differential_Motor(void)
 {
     if (steering_Error>=0)
     {
+        //我艹有bug
         speed_Target2 = speed_Target + Differential_Ratio*steering_Target*4.0f/1062.5f;//左轮目标速度（m/s）
         speed_Target1 = speed_Target - InnerSide_Ratio*Differential_Ratio*steering_Target*4.0f/1062.5f;
 //        speed_Target2 = speed_Target + steering_Error/600*(speed_Target+Differential_Ratio)/(1.2f+1);//左轮目标速度（m/s）
