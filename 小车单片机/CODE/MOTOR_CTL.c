494bb7384ca8387db44724774cd5d02029f1d19d
PLZ <2436478810@qq.com>
2022-07-04 09:21:02 +0800

    调整Lowest回到2.34m/s；减小Highest前瞻

--- 小车单片机/CODE/MOTOR_CTL.c
+++ 小车单片机/CODE/MOTOR_CTL.c
@@ -234,7 +234,7 @@ void Update_Speed_Mode(void)
             BANGBANG_DOWN = 0.2;
 
             speed_Target_Highest = 1.5*3.6f;
-            SightForward_Highest = 0.35f;
+            SightForward_Highest = 0.32f;
             InnerSide_Ratio_Highest = 1.15f;
             Steering_PID_Highest[0]=0.12f;Steering_PID_Highest[1]=0;Steering_PID_Highest[2]=1.0f;
 
@@ -251,7 +251,7 @@ void Update_Speed_Mode(void)
             Steering_PID_Low[0]=0.12f;Steering_PID_Low[1]=0;Steering_PID_Low[2]=1.0f;
 
 
-            speed_Target_Lowest = 2.8f;//¼´2.5
+            speed_Target_Lowest = 2.65f;//¼´2.34
             SightForward_Lowest = 0.32f;
             InnerSide_Ratio_Lowest = 1.10f;
             Steering_PID_Lowest[0]=0.12f;Steering_PID_Lowest[1]=0;Steering_PID_Lowest[2]=1.0f;
