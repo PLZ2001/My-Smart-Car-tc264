65c0eb48d10feebe0d561e52da618b7b634532b2
PLZ <2436478810@qq.com>
2022-08-13 20:30:24 +0800

    三岔路延时增加

--- 小车单片机/CODE/MOTOR_CTL.c
+++ 小车单片机/CODE/MOTOR_CTL.c
@@ -295,7 +295,7 @@ void Update_Speed_Mode(void)
             T_Line = 0.09f+0.09f;
 
             threeRoads_RightTime = 0.6f;//0.25f;
-            ThreeeRoad_Delay = 0.06f;
+            ThreeeRoad_Delay = 0.09f;
 
             Highest_Distance = 0.1f;
 
